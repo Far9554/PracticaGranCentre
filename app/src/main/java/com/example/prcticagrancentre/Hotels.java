8a5584aefcb78710e759a08aa71eedd5e754ee49
Junyao <jun-yao.yan@educem.net>
2021-11-17 10:00:50 +0100

    ha

--- app/src/main/java/com/example/prcticagrancentre/Hotels.java
+++ app/src/main/java/com/example/prcticagrancentre/Hotels.java
@@ -3,12 +3,18 @@ package com.example.prcticagrancentre;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Hotels extends AppCompatActivity {
 
+    Spinner spiner=findViewById(R.id.spinnerHotel);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotels);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Hotels, android.R.layout.simple_spinner_item);
+        spiner.setAdapter(adapter);
     }
 }
