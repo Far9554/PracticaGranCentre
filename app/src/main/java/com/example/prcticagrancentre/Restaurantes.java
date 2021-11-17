7babc564c5c35f5798072848409d8527ff274462
Far9554 <marioleoncarrizo@gmail.com>
2021-11-17 09:52:31 +0100

    yes

--- app/src/main/java/com/example/prcticagrancentre/Restaurantes.java
+++ app/src/main/java/com/example/prcticagrancentre/Restaurantes.java
@@ -3,12 +3,22 @@ package com.example.prcticagrancentre;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 public class Restaurantes extends AppCompatActivity {
+
+    Spinner opciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurantes);
+
+        opciones = (Spinner) findViewById(R.id.sp01);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Restaurants, android.R.layout.simple_spinner_item);
+        opciones.setAdapter(adapter);
     }
 }
