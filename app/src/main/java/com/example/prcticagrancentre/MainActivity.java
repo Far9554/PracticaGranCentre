a53514819530c46e1cdc6ec1e596fc28119132d6
Junyao <jun-yao.yan@educem.net>
2021-11-17 10:13:40 +0100

    ha

--- app/src/main/java/com/example/prcticagrancentre/MainActivity.java
+++ app/src/main/java/com/example/prcticagrancentre/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.prcticagrancentre;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton hotel=findViewById(R.id.btnHotels);
+    ImageView movie=findViewById(R.id.btnMovies);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hotel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentHotel = new Intent(getApplicationContext(), Hotels.class);
+                startActivity(intentHotel);
+            }
+        });
     }
+
 }
