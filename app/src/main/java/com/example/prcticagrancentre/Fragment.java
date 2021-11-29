d52edcdfb4693530aa0b0a2b45d8dea8644c8644
Junyao <jyan14duca@gmail.com>
2021-11-29 10:14:34 +0100

    an

--- app/src/main/java/com/example/prcticagrancentre/Fragment.java
+++ app/src/main/java/com/example/prcticagrancentre/Fragment.java
@@ -1,22 +1,39 @@
 package com.example.prcticagrancentre;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Context;
-import android.os.Bundle;
-import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Fragment extends androidx.fragment.app.Fragment {
-    @Nullable
+public class Fragment extends ArrayAdapter<String> {
+    private static LayoutInflater inflater=null;
+    Context context;
+    String[] nom;
+    String[] desc;
+    int[] image;
+
+    public Fragment(Activity context, String[] nom, int[] image, String[] desc) {
+        super(context, R.layout.list_fragment, nom);
+        this.context=context;
+        this.nom=nom;
+        this.desc=desc;
+        this.image=image;
+        inflater =(LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+    }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_fragment, container);
-
-        return view;
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View vista=inflater.inflate(R.layout.list_fragment,null,true);
+        TextView titulo=(TextView) vista.findViewById(R.id.textTi);
+        TextView Descrip = (TextView) vista.findViewById(R.id.textDesc);
+        ImageView imagen = (ImageView) vista.findViewById(R.id.imgm);
+        titulo.setText(nom[i]);
+        imagen.setImageResource(image[i]);
+        Descrip.setText(desc[i]);
+        return vista;
     }
 }
--- app/src/main/java/com/example/prcticagrancentre/Movies.java
+++ app/src/main/java/com/example/prcticagrancentre/Movies.java
@@ -3,12 +3,26 @@ package com.example.prcticagrancentre;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class Movies extends AppCompatActivity {
 
+    ListView listm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+
+        String[] nomM =getResources().getStringArray(R.array.movieTi);
+        int[] imagem = { R.drawable.dune, R.drawable.alerta, R.drawable.free,R.drawable.escuadron};
+        String[] movieDesc =getResources().getStringArray(R.array.movieDesc);
+
+        listm=(ListView) findViewById(R.id.listmovie);
+        listm.setAdapter(new Fragment(this,nomM,imagem,movieDesc));
     }
 }
